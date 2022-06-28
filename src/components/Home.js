00186e3377acfb0c3d92b0dbf0d5c7ba5572bf35
Tish Faroul <lfaroul14@gmail.com>
2022-06-27 22:37:26 -0400

    added css styling to home title

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,13 @@ function Home() {
   return (
     <div className='Home'>
 
-<iframe src='https://my.spline.design/devicecloudcopy-125c88fb8cc7b6ec0e9251ae58e053db/' frameborder='0' width='100%' height='100%'></iframe>
+        <div className='home_title'>
+            <h4> Welcome to Tish Chat</h4>
+   
+        </div>
+           
+            <iframe src='https://my.spline.design/devicecloudcopy-125c88fb8cc7b6ec0e9251ae58e053db/' 
+            frameborder='0' width='100%' height='100%'></iframe>
 
     </div>
   )
