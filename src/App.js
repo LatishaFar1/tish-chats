e0f82a8c88020530db9c20b7527f3f0716a73b14
Tish Faroul <lfaroul14@gmail.com>
2022-06-27 23:05:39 -0400

    started working on navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import Home from './components/Home';
+import Chat from './components/Chat';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div>
-      <NavBar/>
       <Router>
+      <NavBar/>
         <Routes>
           <Route  path="/" element={<Home />} />
-
+          <Route  path="/chat" element={<Chat/>} />
 
 
         </Routes>
 
       </Router>
-      <Footer/>
-   
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+        <div className='links'>
+            <Link to="/">Home</Link>
+            <Link to="/chat">Chat</Link>
+
+        </div>
+
+
+    </div>
   )
 }
 
