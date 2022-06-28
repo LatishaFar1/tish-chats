b65c13f209c16c5fa0384bf1c8957f05db45b3ee
Tish Faroul <lfaroul14@gmail.com>
2022-06-27 22:24:29 -0400

    added spline 3d to home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
 import './App.css';
+import Home from './components/Home';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <NavBar/>
+      <Router>
+        <Routes>
+          <Route  path="/" element={<Home />} />
+
+
+
+        </Routes>
+
+      </Router>
+      <Footer/>
    
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='Home'>
+
+<iframe src='https://my.spline.design/devicecloudcopy-125c88fb8cc7b6ec0e9251ae58e053db/' frameborder='0' width='100%' height='100%'></iframe>
+
+    </div>
+  )
+}
+
+export default Home
