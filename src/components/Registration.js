467a5be4b394d9b26cdd4feec1683efee1d73c86
Tish Faroul <lfaroul14@gmail.com>
2022-07-11 20:34:39 -0400

    finished registration form and set up working auth

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,24 +1,77 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth, db} from "../firebase";
+import {setDoc, doc, Timestamp} from "firebase/firestore";
 
 function Registration() {
+
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: null
+    });
+
+
+    const {name, email, password, error} = data;
+
+    const handleChange = e => {
+        setData({...data, [e.target.name]: e.target.value })
+    }
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        setData({ ...data, error: null });
+        if (!name || !email || !password) {
+          setData({ ...data, error: "Please fill out each line" });
+        }
+        try {
+          const result = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+          );
+          console.log(result.user);
+          await setDoc(doc(db, "users", result.user.uid), {
+            uid: result.user.uid,
+            name,
+            email,
+            createdAt: Timestamp.fromDate(new Date()),
+            isOnline: true,
+          });
+          setData({
+            name: "",
+            email: "",
+            password: "",
+            error: null,
+          });
+        } catch (error) {
+          setData({ ...data, error: error.message});
+        }
+      };
+
   return (
     <div className='register'>
-        <form>
             <h1 className='register-h1'>Register</h1>
+        <form onSubmit={handleRegister}>
             <br/>
             <div className='form-container'>
                 <label name="name">Name</label>
-                <input type="text" name="name" />
+                <input type="text" name="name" value={name} onChange={handleChange}/>
             </div>
 
             <div className='form-container'>
                 <label name="email">Email</label>
-                <input type="text" name="email" />
+                <input type="text" name="email"  value={email} onChange={handleChange}/>
             </div>
 
             <div className='form-container'>
                 <label name="password">Password</label>
-                <input type="text" name="password" />
+                <input type="text" name="password"  value={password} onChange={handleChange}/>
+            </div>
+            {error ? <h3 className='error'>{error}</h3> : null}
+            <div className='register-button'>
+                <button className='reg-button'>Register</button>
             </div>
         </form>
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,15 +10,19 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDhm3vDk0-WZXub9qbBX-J_LMSUlfUOSmI",
+  authDomain: "tish-chats.firebaseapp.com",
+  projectId: "tish-chats",
+  databaseURL: "http://tish-chats.firebaseio.com",
+  storageBucket: "tish-chats.appspot.com",
+  messagingSenderId:"126878923393",
+  appId: "1:126878923393:web:cff2ade2a2746946a32b80",
   measurementId: "G-RYEHET1301"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app);
+
+export {auth, db};
